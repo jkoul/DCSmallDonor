9f9b4b688211edf9a7ddfa5242d9a379cf675b04
Jeremy Koulish <jkoulish@gmail.com>
2016-01-04 00:43:10 -0500

    fix for heroku asset pipeline

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,5 +1,5 @@
 class OrganizationsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def index
     @organizations = Organization.all.order(:name)
